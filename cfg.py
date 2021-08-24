4ee27922bf6638c5c203cdce8d6b4f0ff14728de
bchandos <bill.chandos@gmail.com>
2021-08-24 08:10:36 -0700

    actually commit db changes

--- cfg.py
+++ cfg.py
@@ -3,26 +3,37 @@ import sqlite3
 
 import aiosql
 
-conn = sqlite3.connect('db.sqlite')
-conn.row_factory = sqlite3.Row
 
-schema = aiosql.from_path('sql/schema.sql', 'sqlite3')
-schema.create_schema(conn)
 
 queries = aiosql.from_path('sql/repos.sql', 'sqlite3')
 
 Repo = namedtuple('Repo', ['id', 'name', 'path'])
 
+def get_conn():
+    conn = sqlite3.connect('db.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 def load_repos():
-    repos = [
-        Repo(id=r['id'], name=r['name'], path=r['path'])
-        for r in queries.get_all_repos(conn)
-    ]
+    conn = get_conn()
+    with conn:
+        repos = [
+            Repo(id=r['id'], name=r['name'], path=r['path'])
+            for r in queries.get_all_repos(conn)
+        ]
 
     return repos
 
 def create_repo(name, path, cache_logs):
-    queries.create_repo(conn, name, path, cache_logs)
+    conn = get_conn()
+    with conn:
+        queries.create_repo(conn, name, path, cache_logs)
     return load_repos()
+
+
+schema = aiosql.from_path('sql/schema.sql', 'sqlite3')
+conn = get_conn()
+with conn:
+    schema.create_schema(conn)
 
 repos = load_repos()
