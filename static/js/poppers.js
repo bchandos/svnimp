d42e9fe61a25e50678cd5763caec4d0d9ae49783
bchandos <bill.chandos@gmail.com>
2021-09-19 08:40:04 -0700

    Convert JS poppers into Web Components

--- static/js/poppers.js
+++ static/js/poppers.js
@@ -1,134 +1,147 @@
-// Add a universal backdrop to the body the will only be displayed 
-// when a popper is open and clicking on it will close all poppers
+class PopperBox extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({mode: 'open'}); // sets and returns 'this.shadowRoot'
+        // Apply external styles to the shadow dom
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', '/static/css/poppers.css');
 
-// TODO: refine positioning so it doesn't render outside the viewport
-
-const popperBackDrop = document.createElement('div');
-popperBackDrop.id = 'popper-backdrop';
-document.body.appendChild(popperBackDrop);
-
-const popperFit = (desiredPosition, popper, pBox) => {
-    let pos, pTop, pLeft;
-    const btnHeight = pBox.clientHeight;
-    const btnWidth = pBox.clientWidth;
-    const popperHeight = popper.clientHeight;
-    const popperWidth = popper.clientWidth;
-    switch (desiredPosition) {
-        case 'bottom':
-            pTop = btnHeight + 8;
-            pLeft = -(popperWidth/2) + (btnWidth/2);
-            pos = 'bottom';
-            break;
-        case 'top':
-            pTop = -popperHeight - 8;
-            pLeft = -(popperWidth/2) + (btnWidth/2);
-            pos = 'top';
-            break;
-        case 'left':
-            pTop = -(popperHeight/2) + 8;
-            pLeft = -popperWidth - 8;
-            pos = 'left';
-            break;
-        case 'right':
-            pTop = -(popperHeight/2) + 8;
-            pLeft = btnWidth + 8;
-            pos = 'right';
-            break;
+        // Event listener that needs removal (https://stackoverflow.com/a/22870717)
+        this._zPopBack = this.zPopBack.bind(this);
+        
+        // Create backdrop
+        const popperBackDrop = document.createElement('div'); 
+        popperBackDrop.id = 'popper-backdrop';
+        popperBackDrop.addEventListener('click', (e) => this.closePoppers(e));
+        
+        // Create the trigger button element
+        const triggerBtn = document.createElement('button');
+        triggerBtn.innerText = this.dataset.triggerText;
+        triggerBtn.type = 'button';
+        triggerBtn.classList.add('popper-button');
+        triggerBtn.addEventListener('click', (e) => this.openPopper(e));
+        
+        // Create the actual popper
+        const popper = document.createElement('div');
+        popper.classList.add('popper');
+        popper.dataset.open = 'false';
+        popper.dataset.position = this.dataset.position || 'left';
+        popper.innerHTML = this.innerHTML;
+        
+        // Remove the innerHTML to make inspection less confusing
+        this.innerHTML = null;
+        
+        // Create box containing trigger and popper
+        const popperBox = document.createElement('span');
+        popperBox.classList = this.classList;
+        popperBox.classList.add('popper-box');
+        popperBox.appendChild(triggerBtn);
+        popperBox.appendChild(popper);
+        
+        // Attach the created elements to the shadow dom
+        shadow.appendChild(linkElem);
+        shadow.appendChild(popperBackDrop);
+        shadow.appendChild(popperBox);
     }
-    return { pos, pTop, pLeft }
-}
 
-const createPopper = (elem) => {
-    const trigger = elem.dataset.triggerText || elem.querySelectorAll('span.popper-trigger').innerHTML;
-    const triggerBtn = document.createElement('button');
-    triggerBtn.innerText = trigger;
-    triggerBtn.type = 'button';
-    triggerBtn.classList.add('popper-button');
-    triggerBtn.addEventListener('click', openPopper);
-    const popper = document.createElement('div');
-    popper.classList.add('popper');
-    popper.dataset.open = 'false';
-    popper.dataset.position = elem.dataset.position || 'left';
-    popper.innerHTML = elem.innerHTML;
-    
-    const popperBox = document.createElement('span');
-    popperBox.classList = elem.classList;
-    popperBox.classList.add('popper-box');
-    popperBox.appendChild(triggerBtn);
-    popperBox.appendChild(popper);
-    elem.replaceWith(popperBox);
-}
-
-
-const createPoppers = () => {
-    const allDialogElems = document.querySelectorAll('popper');
-    for (let elem of allDialogElems) {
-        createPopper(elem);
+    popperFit(desiredPosition, popper, pBox) {
+        /*  Given a desired position, a popper element, and its containing
+            box, return the appropriate `top` and `left` positioning.
+        */
+        let pos, pTop, pLeft;
+        const btnHeight = pBox.clientHeight;
+        const btnWidth = pBox.clientWidth;
+        const popperHeight = popper.clientHeight;
+        const popperWidth = popper.clientWidth;
+        switch (desiredPosition) {
+            case 'bottom':
+                pTop = btnHeight + 8;
+                pLeft = -(popperWidth/2) + (btnWidth/2);
+                pos = 'bottom';
+                break;
+            case 'top':
+                pTop = -popperHeight - 8;
+                pLeft = -(popperWidth/2) + (btnWidth/2);
+                pos = 'top';
+                break;
+            case 'left':
+                pTop = -(popperHeight/2) + 8;
+                pLeft = -popperWidth - 8;
+                pos = 'left';
+                break;
+            case 'right':
+                pTop = -(popperHeight/2) + 8;
+                pLeft = btnWidth + 8;
+                pos = 'right';
+                break;
+        }
+        return { pos, pTop, pLeft }
     }
-}
 
-const zPopBack = (e) => {
-    e.target.style.zIndex = '-1';
-    e.target.style = null;
-}
-
-const openPopper = (e) => {
-    const popper = e.currentTarget.nextSibling;
-    const pBox = e.currentTarget;
-    popper.style.display = 'block';
-    console.log(popper.style);
-    let fit = popperFit(popper.dataset.position, popper, pBox);
-    console.log(popper.style);
-    popper.style.top = `${fit.pTop}px`;
-    popper.style.left = `${fit.pLeft}px`;
-    const pRect = popper.getBoundingClientRect();
-    // If the popper is going to render offscreen, find another location for it.
-    // This will only handle a single case, and should handle all cases...
-    if (pRect.x < 0) {
-        popper.style.top, popper.style.left = null;
-        fit = popperFit('right', popper, pBox);
+    openPopper(e) {
+        // Open a popper
+        const popper = e.currentTarget.nextSibling;
+        const pBox = e.currentTarget;
+        popper.style.display = 'block';
+        let fit = this.popperFit(popper.dataset.position, popper, pBox);
         popper.style.top = `${fit.pTop}px`;
         popper.style.left = `${fit.pLeft}px`;
-    } else if ((pRect.x + pRect.width) > document.documentElement.clientWidth) {
-        popper.style.top, popper.style.left = null;
-        fit = popperFit('left', popper, pBox);
-        popper.style.top = `${fit.pTop}px`;
-        popper.style.left = `${fit.pLeft}px`;
-    } else if (pRect.y < 0) {
-        popper.style.top, popper.style.left = null;
-        fit = popperFit('bottom', popper, pBox);
-        popper.style.top = `${fit.pTop}px`;
-        popper.style.left = `${fit.pLeft}px`;
-    } else if ((pRect.y + pRect.height) > document.documentElement.clientHeight) {
-        popper.style.top, popper.style.left = null;
-        fit = popperFit('top', popper, pBox);
-        popper.style.top = `${fit.pTop}px`;
-        popper.style.left = `${fit.pLeft}px`;
+        const pRect = popper.getBoundingClientRect();
+        // If the popper is going to render offscreen, find another location for it.
+        // This will only handle a single case, and should handle all cases...
+        if (pRect.x < 0) {
+            popper.style.top, popper.style.left = null;
+            fit = this.popperFit('right', popper, pBox);
+            popper.style.top = `${fit.pTop}px`;
+            popper.style.left = `${fit.pLeft}px`;
+        } else if ((pRect.x + pRect.width) > document.documentElement.clientWidth) {
+            popper.style.top, popper.style.left = null;
+            fit = this.popperFit('left', popper, pBox);
+            popper.style.top = `${fit.pTop}px`;
+            popper.style.left = `${fit.pLeft}px`;
+        } else if (pRect.y < 0) {
+            popper.style.top, popper.style.left = null;
+            fit = this.popperFit('bottom', popper, pBox);
+            popper.style.top = `${fit.pTop}px`;
+            popper.style.left = `${fit.pLeft}px`;
+        } else if ((pRect.y + pRect.height) > document.documentElement.clientHeight) {
+            popper.style.top, popper.style.left = null;
+            fit = this.popperFit('top', popper, pBox);
+            popper.style.top = `${fit.pTop}px`;
+            popper.style.left = `${fit.pLeft}px`;
+        }
+        popper.dataset.position = fit.pos;
+        popper.classList.add(`pos-${fit.pos}`);
+        popper.removeEventListener('transitionend', this._zPopBack);
+        popper.style.opacity = '1';
+        popper.style.zIndex = '10';
+        popper.dataset.open = 'true';
+        const popperBackDrop = this.shadowRoot.getElementById('popper-backdrop');
+        popperBackDrop.style.opacity = '1';
+        popperBackDrop.style.zIndex = '5';
+        popperBackDrop.removeEventListener('transitionend', this._zPopBack);
     }
-    popper.dataset.position = fit.pos;
-    popper.classList.add(`pos-${fit.pos}`);
-    popper.removeEventListener('transitionend', zPopBack);
-    popper.style.opacity = '1';
-    popper.style.zIndex = '10';
-    popper.dataset.open = 'true';
-    
-    popperBackDrop.style.opacity = '1';
-    popperBackDrop.style.zIndex = '5';
-    popperBackDrop.removeEventListener('transitionend', zPopBack);
-}
 
+    zPopBack(e) {
+        /*  To enable animations, transitionend event handler to lower
+            element's z-index and clear added styles so `display: none`
+            can take precedence
+        */
+        e.target.style.zIndex = '-1';
+        e.target.style = null;
+    }
 
-const closePoppers = () => {
-    const openPoppers = document.querySelectorAll('div.popper[data-open="true"]');
-    for (let p of openPoppers) {
-        p.addEventListener('transitionend', zPopBack);
+    closePoppers(e) {
+        // Close a popper
+        const p = this.shadowRoot.querySelector('div.popper');
+        p.addEventListener('transitionend', this._zPopBack);
         p.style.opacity = '0';
         p.dataset.open = 'false';
+        e.currentTarget.addEventListener('transitionend', this._zPopBack);
+        e.currentTarget.style.opacity = '0';
     }
-    popperBackDrop.addEventListener('transitionend', zPopBack);
-    popperBackDrop.style.opacity = '0';
+
 }
 
-popperBackDrop.addEventListener('click', closePoppers);
-
-createPoppers();
+customElements.define('popper-box', PopperBox);
